014a19da1beded3a5d14c791b563bca66010184b
Alex <erhardtalex@gmail.com>
2024-07-13 13:02:23 -0500

    add nav

---
+++ dnd-app/frontend/src/NavBar.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import {
+	Dropdown,
+	DropdownItem,
+	DropdownMenu,
+	DropdownToggle,
+	Nav,
+	Navbar,
+} from "reactstrap";
+
+import "./assets/styles/NavBar.css";
+
+export function NavBar() {
+	const [dropdownOpen, setDropdownOpen] = useState(false);
+
+	const toggle = () => setDropdownOpen((prevState) => !prevState);
+	return (
+		<Navbar className="navbar fixed-top navbar-light" expand="md">
+			<NavLink exact to="/" className="navbar-brand">
+				<img src="/logo.png" alt="" />
+			</NavLink>
+			<Nav className="ml-auto" navbar>
+				<Dropdown
+					isOpen={dropdownOpen}
+					toggle={toggle}
+					direction="down"
+				>
+					<DropdownToggle color="none">
+						<img src="/3-horizontal-lines.svg" alt="settings" />
+					</DropdownToggle>
+					<DropdownMenu>
+						<DropdownItem header>Account</DropdownItem>
+						<DropdownItem>
+							<NavLink to="/">Login/Sign-up</NavLink>
+						</DropdownItem>
+					</DropdownMenu>
+				</Dropdown>
+			</Nav>
+		</Navbar>
+	);
+}
