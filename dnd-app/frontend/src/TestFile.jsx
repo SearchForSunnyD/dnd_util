3f1b4a31e531e908411e718c0db6b225c1388e9d
Alex <erhardtalex@gmail.com>
2024-07-15 19:33:28 -0500

    start feats

--- dnd-app/frontend/src/TestFile.jsx
+++ dnd-app/frontend/src/TestFile.jsx
@@ -1,7 +1,57 @@
+import {
+	Badge,
+	Card,
+	CardBody,
+	CardImg,
+	CardSubtitle,
+	CardText,
+	CardTitle,
+	Col,
+	Container,
+	ListGroup,
+	ListGroupItem,
+	Row,
+} from "reactstrap";
 import { data } from "./tools";
 
+import("./assets/styles/Details.css");
 
 export function TestFile() {
-
-	
+	return (
+		<Container>
+			<Row>
+				<CardImg src="./icons/feats.png" />
+				<Col>
+					<Card>
+						<CardBody>
+							<CardTitle tag="h1">{data.name}</CardTitle>
+							<CardSubtitle>
+								<strong>{data.prerequisite}</strong>
+							</CardSubtitle>
+							<hr />
+							<CardText>{data.desc}</CardText>
+							<ListGroup numbered flush className="fw-lighter">
+								{data.effects_desc.map((effect, index) => (
+									<ListGroupItem
+										className="small"
+										key={index}
+									>
+										{effect}
+									</ListGroupItem>
+								))}
+							</ListGroup>
+							<CardText className="mt-3">
+								<strong>Source:</strong>{" "}
+								<Badge pill color="warning">
+									<a href={data.document__url}>
+										{data.document__title}
+									</a>
+								</Badge>
+							</CardText>
+						</CardBody>
+					</Card>
+				</Col>
+			</Row>
+		</Container>
+	);
 }
