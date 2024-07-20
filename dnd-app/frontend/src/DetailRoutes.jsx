b38916d1e562781a680ced1dd2084de61d037270
Alex <erhardtalex@gmail.com>
2024-07-20 15:33:01 -0500

    add router

---
+++ dnd-app/frontend/src/DetailRoutes.jsx
@@ -0,0 +1,33 @@
+import { Route } from "react-router-dom";
+
+import { Monster } from "./Monster";
+import { SpellCard } from "./SpellCard";
+import { MagicItems } from "./MagicItems";
+import { Races } from "./Races";
+import { Armor } from "./Armor";
+import { Feats } from "./Feats";
+import { Backgrounds } from "./Backgrounds";
+import { BasicCard } from "./BasicCard";
+
+export function DetailRoutes() {
+	return (
+		<>
+			<Route path="/monsters/:slug" element={<Monster />} />
+			<Route path="/spells/:slug" element={<SpellCard />} />
+			<Route path="/magicitems/:slug" element={<MagicItems />} />
+			<Route path="/races/:slug" element={<Races />} />
+			<Route path="/armor/:slug" element={<Armor />} />
+			<Route path="/feats/:slug" element={<Feats />} />
+			<Route path="/backgrounds/:slug" element={<Backgrounds />} />
+			<Route path="/planes/:slug" element={<BasicCard type="planes" />} />
+			<Route
+				path="/conditions/:slug"
+				element={<BasicCard type="conditions" />}
+			/>
+			<Route
+				path="/sections/:slug"
+				element={<BasicCard type="sections" />}
+			/>
+		</>
+	);
+}
--- dnd-app/frontend/src/RouteList.jsx
+++ dnd-app/frontend/src/RouteList.jsx
@@ -1,17 +1,20 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 
 import { UserRoutes } from "./UserRoutes";
+import { DetailRoutes } from "./DetailRoutes";
 import { SearchResult } from "./SearchResults";
+
 
 
 export function RouteList() {
 	return (
-		<main>
+		<>
 			<Routes>
 				{UserRoutes()}
+				{DetailRoutes()}
 				<Route path="/*" element={<Navigate to="/" />} />
 				<Route path="/search-results" element={<SearchResult />} />
 			</Routes>
-		</main>
+		</>
 	);
 }
