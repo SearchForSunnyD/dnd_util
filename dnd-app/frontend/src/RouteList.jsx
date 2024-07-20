24eb7c04071cae273f93ea58bc92d38b4b2a6a80
Alex <erhardtalex@gmail.com>
2024-07-20 09:36:57 -0500

    consolidate

--- dnd-app/frontend/src/RouteList.jsx
+++ dnd-app/frontend/src/RouteList.jsx
@@ -1,25 +1,13 @@
-import { Route, Routes, Navigate } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 
-import { LoginSignup } from "./LoginSignup";
-import { Logout } from "./Logout";
-import { Profile } from "./Profile";
-import { PrivateRoute } from "./PrivateRoute";
+import { UserRoutes } from "./UserRoutes";
+
 
 export function RouteList() {
 	return (
 		<main>
 			<Routes>
-				<Route path="/login/signup" element={<LoginSignup />} />
-				<Route
-					path="/profile"
-					element={<PrivateRoute element={<Profile />} />}
-				/>
-				<Route
-					path="/logout"
-					element={<PrivateRoute element={<Logout />} />}
-				/>
-				<Route path="/login" element={<Navigate to="/" />} />
-				<Route path="/signup" element={<Navigate to="/" />} />
+				{UserRoutes()}
 				<Route path="/*" element={<Navigate to="/" />} />
 			</Routes>
 		</main>
---
+++ dnd-app/frontend/src/UserRoutes.jsx
@@ -0,0 +1,24 @@
+import { Navigate, Route } from "react-router-dom";
+
+import { LoginSignup } from "./LoginSignup";
+import { Logout } from "./Logout";
+import { PrivateRoute } from "./PrivateRoute";
+import { Profile } from "./Profile";
+
+export function UserRoutes() {
+  return (
+		<>
+			<Route path="/login/signup" element={<LoginSignup />} />
+			<Route
+				path="/profile"
+				element={<PrivateRoute element={<Profile />} />}
+			/>
+			<Route
+				path="/logout"
+				element={<PrivateRoute element={<Logout />} />}
+			/>
+			<Route path="/login" element={<Navigate to="/" />} />
+			<Route path="/signup" element={<Navigate to="/" />} />
+		</>
+  );
+}
