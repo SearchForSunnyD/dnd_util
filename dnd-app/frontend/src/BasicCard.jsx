c6860fc373a037650d792e352a7c927752773ce6
Alex <erhardtalex@gmail.com>
2024-07-20 15:32:24 -0500

    finish basic card

--- dnd-app/frontend/src/BasicCard.jsx
+++ dnd-app/frontend/src/BasicCard.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import {
 	Badge,
 	Card,
@@ -8,33 +10,59 @@ import {
 	Col,
 	Container,
 	Row,
+	Spinner,
 } from "reactstrap";
+import DndApi from "./api";
 
 import("./assets/styles/Details.css");
 
-export function BasicCard({ data, img_url }) {
+export function BasicCard({type}) {
+	const { slug } = useParams();
+	const [loading, setLoading] = useState(true);
+	const [data, setData] = useState(null);
+
+	useEffect(() => {
+		async function getInfo() {
+			setLoading(true);
+			let res = await DndApi.getFromExternal({ type, slug });
+			setData(res);
+			setLoading(false);
+		}
+		getInfo();
+	}, [slug, type]);
+
 	return (
 		<Container>
-			<Row>
-				<CardImg src={`./icons/${img_url}.png`} />
-				<Col>
-					<Card className="info">
-						<CardBody>
-							<CardTitle tag="h1">{data.name}</CardTitle>
-							<CardText>{data.type}</CardText>
-							<CardText>{data.desc}</CardText>
-							<CardText className="mt-3">
-								<strong>Source:</strong>{" "}
-								<Badge pill color="warning">
-									<a href={data.document__url}>
-										{data.document__title}
-									</a>
-								</Badge>
-							</CardText>
-						</CardBody>
-					</Card>
-				</Col>
-			</Row>
+			{loading ? (
+				<Container fluid className="mx-auto">
+					<Spinner type="grow" />
+					<Spinner type="grow" />
+					<Spinner type="grow" />
+					<Spinner type="grow" />
+					<Spinner type="grow" />
+				</Container>
+			) : (
+				<Row>
+					<CardImg src={`/icons/${type}.png`} />
+					<Col>
+						<Card className="info">
+							<CardBody>
+								<CardTitle tag="h1">{data.name}</CardTitle>
+								<CardText>{data.type}</CardText>
+								<CardText>{data.desc}</CardText>
+								<CardText className="mt-3">
+									<strong>Source:</strong>{" "}
+									<Badge pill color="warning">
+										<a href={data.document__url}>
+											{data.document__title}
+										</a>
+									</Badge>
+								</CardText>
+							</CardBody>
+						</Card>
+					</Col>
+				</Row>
+			)}
 		</Container>
 	);
 }
